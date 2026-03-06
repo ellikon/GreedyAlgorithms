97cc08a01d431c95739ba47f33a3c8fadb3e321b
ellikon <robbie@millertribe.com>
2026-03-06 11:19:17 -0500

    Implemented basic FIFO cache

--- cacheRequests.py
+++ cacheRequests.py
@@ -1,4 +1,26 @@
 capacity = 3
 requests = [1, 2, 3, 1, 1, 3, 4, 5, 2, 1]
 
-cache = [0] * capacity
+cache = []
+
+
+fifo = []
+fifo_misses = 0
+
+for id in requests:
+    if len(cache) < 3:
+        cache.append(id)
+        fifo.append(id)
+        continue
+
+    if id in cache:
+        continue
+
+    if id not in cache:
+        first = fifo.pop(0)
+        cache.remove(first)
+        cache.append(id)
+        fifo.append(id)
+        fifo_misses += 1
+
+print(f"FIFO\t: {fifo_misses}")
