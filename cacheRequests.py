8be7f4d487adb95afc0fa4538c367637a2368e9c
ordinalsoul <55070893+ordinalsoul@users.noreply.github.com>
2026-03-06 13:12:08 -0500

    OPTFF implementation

--- cacheRequests.py
+++ cacheRequests.py
@@ -51,5 +51,51 @@ def lru(capacity, requests):
             lru_misses += 1
     return lru_misses
 
+
+def OPTFF(capacity, requests):
+    size = len(requests)
+
+    next_time = [float('inf')]* size
+    seen = {}
+
+    for i in range(size-1, -1, -1):
+        item = requests[i]
+        if item in seen:
+            next_time[i] = seen[item]
+        seen[item] = i
+
+    cache = []
+    next_in_cache = {}
+    misses = 0
+
+    for i, item in enumerate(requests):
+        if item in cache:
+            next_in_cache[item] = next_time[i]
+            continue
+
+        misses += 1
+
+        if len(cache) < capacity:
+            cache.append(item)
+            next_in_cache[item] = next_time[i]
+        else:
+            eviction = None
+            farthest = -1
+
+            for cached in cache:
+                far = next_in_cache[cached]
+                if far > farthest:
+                    farthest = far
+                    eviction = cached
+
+            cache.remove(eviction)
+            del next_in_cache[eviction]
+
+            cache.append(item)
+            next_in_cache[item] = next_time[i]
+
+    return misses
+
 print(f"FIFO\t: {fifo(capacity, requests)}")
 print(f"LRU\t: {lru(capacity, requests)}")
+print(f"OPTFF\t: {OPTFF(capacity, requests)}")
