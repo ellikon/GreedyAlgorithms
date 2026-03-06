b9f97fd461eafe88cf8205b7e7f9ca1fec66dedb
ellikon <robbie@millertribe.com>
2026-03-06 11:26:44 -0500

    Added LRU cache

--- cacheRequests.py
+++ cacheRequests.py
@@ -2,8 +2,6 @@ capacity = 3
 requests = [1, 2, 3, 1, 1, 3, 4, 5, 2, 1]
 
 cache = []
-
-
 fifo = []
 fifo_misses = 0
 
@@ -23,4 +21,28 @@ for id in requests:
         fifo.append(id)
         fifo_misses += 1
 
+
+cache = []
+lru = []
+lru_misses = 0
+
+for id in requests:
+    if len(cache) < 3:
+        cache.append(id)
+        lru.append(id)
+        continue
+
+    if id in cache:
+        lru.remove(id)
+        lru.append(id)
+        continue
+
+    if id not in cache:
+        last_used = lru.pop(0)
+        cache.remove(last_used)
+        cache.append(id)
+        lru.append(id)
+        lru_misses += 1
+
 print(f"FIFO\t: {fifo_misses}")
+print(f"LRU\t: {lru_misses}")
