fdcbbc2e878909da29bb4a2031b377c196b57a55
ordinalsoul <55070893+ordinalsoul@users.noreply.github.com>
2026-03-06 11:45:09 -0500

    made LRU and FIFO into functions that return the number of misses to make accessing them easier

--- cacheRequests.py
+++ cacheRequests.py
@@ -1,48 +1,52 @@
 capacity = 3
 requests = [1, 2, 3, 1, 1, 3, 4, 5, 2, 1]
 
-cache = []
-fifo = []
-fifo_misses = 0
 
-for id in requests:
-    if len(cache) < capacity and id not in cache:
-        cache.append(id)
-        fifo.append(id)
-        continue
+def fifo(capacity, requests):
+    cache = []
+    fifo = []
+    fifo_misses = 0
 
-    if id in cache:
-        continue
+    for id in requests:
+        if len(cache) < capacity and id not in cache:
+            cache.append(id)
+            fifo.append(id)
+            continue
 
-    if id not in cache:
-        first = fifo.pop(0)
-        cache.remove(first)
-        cache.append(id)
-        fifo.append(id)
-        fifo_misses += 1
+        if id in cache:
+            continue
 
+        if id not in cache:
+            first = fifo.pop(0)
+            cache.remove(first)
+            cache.append(id)
+            fifo.append(id)
+            fifo_misses += 1
+    return fifo_misses
 
-cache = []
-lru = []
-lru_misses = 0
+def lru(capacity, requests):
+    cache = []
+    lru = []
+    lru_misses = 0
 
-for id in requests:
-    if len(cache) < capacity and id not in cache:
-        cache.append(id)
-        lru.append(id)
-        continue
+    for id in requests:
+        if len(cache) < capacity and id not in cache:
+            cache.append(id)
+            lru.append(id)
+            continue
 
-    if id in cache:
-        lru.remove(id)
-        lru.append(id)
-        continue
+        if id in cache:
+            lru.remove(id)
+            lru.append(id)
+            continue
 
-    if id not in cache:
-        last_used = lru.pop(0)
-        cache.remove(last_used)
-        cache.append(id)
-        lru.append(id)
-        lru_misses += 1
+        if id not in cache:
+            last_used = lru.pop(0)
+            cache.remove(last_used)
+            cache.append(id)
+            lru.append(id)
+            lru_misses += 1
+    return lru_misses
 
-print(f"FIFO\t: {fifo_misses}")
-print(f"LRU\t: {lru_misses}")
+print(f"FIFO\t: {fifo(capacity, requests)}")
+print(f"LRU\t: {lru(capacity, requests)}")
